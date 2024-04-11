05bdd1f340a139b9a7031e27f7e8f93d9e34ac6c
Ravi Raj <raviraj@Ravis-MacBook-Air.local>
2024-04-12 01:00:18 +0530

    Completed Ingredient controller

--- src/main/java/com/ravi/controller/IngredientController.java
+++ src/main/java/com/ravi/controller/IngredientController.java
@@ -3,12 +3,16 @@ package com.ravi.controller;
 import com.ravi.Service.IngredientsService;
 import com.ravi.Service.UserService;
 import com.ravi.model.IngredientCategory;
+import com.ravi.model.IngredientsItems;
 import com.ravi.model.User;
 import com.ravi.request.IngredientCategoryRequest;
+import com.ravi.request.IngredientItemRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin/ingredients")
@@ -28,5 +32,57 @@ public class IngredientController {
         IngredientCategory ingredientCategory = ingredientsService.createIngredientCategory(req.getName(), req.getRestaurantId());
 
         return new ResponseEntity<>(ingredientCategory, HttpStatus.CREATED);
+    }
+
+    @PostMapping()
+    public ResponseEntity<IngredientsItems> createIngredientItem (
+            @RequestBody IngredientItemRequest req,
+            @RequestHeader("Authorization") String jwt
+            ) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+
+        IngredientsItems item = ingredientsService.createIngredientItem(req.getRestaurantId(), req.getName(), req.getCategoryId());
+
+        return new ResponseEntity<>(item, HttpStatus.OK);
+    }
+
+    @PutMapping("/{ingredientItemId}/stock")
+    public ResponseEntity<IngredientsItems> toggleIngredientStock (
+            @PathVariable Long ingredientItemId,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+
+        IngredientsItems item = ingredientsService.toggleAvailability(ingredientItemId);
+
+        return new ResponseEntity<>(item, HttpStatus.OK);
+    }
+
+    @GetMapping("/restaurant/{restaurantId}")
+    public ResponseEntity<List<IngredientsItems>> getRestaurantAllIngredientItems (
+            @PathVariable Long restaurantId,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+
+        List<IngredientsItems> ingredientsItems = ingredientsService.findIngredientItemsByRestaurantId(restaurantId);
+
+        return new ResponseEntity<>(ingredientsItems, HttpStatus.OK);
+    }
+
+    @GetMapping("/restaurant/{restaurantId}/category")
+    public ResponseEntity<List<IngredientCategory>> getRestaurantAllIngredientCategory (
+            @PathVariable Long restaurantId,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+
+        List<IngredientCategory> ingredientCategories = ingredientsService.findIngredientCategoryByRestaurantId(restaurantId);
+
+        return new ResponseEntity<>(ingredientCategories, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/ravi/request/IngredientItemRequest.java
@@ -0,0 +1,14 @@
+package com.ravi.request;
+
+
+import lombok.Data;
+
+@Data
+public class IngredientItemRequest {
+
+    private String name;
+
+    private Long categoryId;
+
+    private Long restaurantId;
+}
